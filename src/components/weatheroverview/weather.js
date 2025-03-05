62a6a7d693e867c1625b1f1ea097335e5a9eb940
neathcmd <ksdf71464@gmail.com>
2025-03-05 16:34:10 +0700

    add about

--- src/components/weatheroverview/weather.js
+++ src/components/weatheroverview/weather.js
@@ -206,7 +206,7 @@ function renderWeather(data) {
                           <div class="flex items-center">
                               <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 sm:h-5 sm:w-5 mr-2 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
-                              </svg>
+              black                </svg>
                               <div>
                                   <p class="text-gray-400 text-xs sm:text-sm">High</p>
                                   <p class="text-sm sm:text-base">${highTemp}${tempUnit}</p>
